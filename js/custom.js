ce9b3df564341f50cf92d8644848bb155a4e75fa
yaster25 <ir.yastrebova@gmail.com>
2020-07-16 21:53:16 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,358 @@
+$(window).on('load', function () {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('body').addClass('ios');
+	} else{
+		$('body').addClass('web');
+	};
+ 
+});
+
+$(document).ready(function(){
+    
+    /* TOP SLIDER*/ 
+    $('.js-top-slider').on('init', function(event, slick){
+        $('.top-slider-dots li').removeClass('active');
+        $('.top-slider-dots li[data-slide="00"]').addClass('active');
+    });
+    
+    $('.js-top-slider').slick({
+            infinite: true,
+            arrows:false,
+            dots:false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade:true,
+            autoplay:true,
+            appendDots:$('.top-slider-dots'),
+            pauseOnFocus:false,
+            pauseOnHover:false            
+        });
+     
+    $('.top-slider-nav-arrow').on('click', function(){
+        $('.js-top-slider').slick('slickNext');
+        return false;
+    });
+     
+     $('.top-slider-nav__slide').on('click', function(){
+         var ds=$(this).attr('data-slide');
+        $('.js-top-slider').slick('slickGoTo', ds);
+         $('.top-slider-nav__slide').removeClass('active');
+         $(this).addClass('active');
+        return false;
+    });
+     
+     $('.top-slider-dots li').on('click', function(){
+         var ds=$(this).attr('data-slide');
+        $('.js-top-slider').slick('slickGoTo', ds);
+         $('.top-slider-dots li').removeClass('active');
+         $(this).addClass('active');
+        return false;
+    });  
+     
+     $('.js-top-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.top-slider-nav__slide').removeClass('active');
+        $('.top-slider-nav__slide[data-slide="0'+nextSlide+'"]').addClass('active');
+         
+        $('.top-slider-dots li').removeClass('active');
+        $('.top-slider-dots li[data-slide="0'+nextSlide+'"]').addClass('active');         
+    });
+    
+    /*END TOP SLIDER*/
+    
+    /*  ADVANTAGES SLIDER*/ 
+        $('.js-slider-advantages').each(function () {
+          $(this).slick({
+            infinite: true,
+            arrows:true,
+            dots:false,
+            slidesToShow:2,
+            slidesToScroll: 1,
+            appendArrows: $(this).parents('.slider-wrapper').find('.slider-arrows'),
+            swipeToSlide:true,
+            centerMode:false,
+            fade:false,
+            variableWidth:true,
+            responsive: [   
+                {
+                  breakpoint: 741,
+                  settings: {
+                    variableWidth:false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                  }
+                }
+              ]
+        });
+    });
+     /* END ADVANTAGES SLIDER*/ 
+    
+    /* Team SLIDER*/ 
+    $('.js-slider-team').slick({
+        infinite: true,
+        arrows:false,
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade:true
+    });
+     
+   
+     $('.slider-team-nav__item').on('click', function(){
+         var ds=$(this).attr('data-slide');
+        $('.js-slider-team').slick('slickGoTo', ds);
+         $('.slider-team-nav__item').removeClass('active');
+         $(this).addClass('active');
+        return false;
+    });
+     
+     
+     $('.js-slider-team').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $('.slider-team-nav__item').removeClass('active');
+        $('.slider-team-nav__item[data-slide="0'+nextSlide+'"]').addClass('active');        
+    });
+    
+    /*END TEAM SLIDER*/
+    
+      /* NICE INPUT*/
+    $(".input-phone").mask("+7 (999) 999-99-99");
+    
+    $('.nice-input__input').blur(function() {
+        
+        if($(this).val().length>0){
+            $(this).addClass('nice-input__input--filled');
+        }
+        else{
+            $(this).removeClass('nice-input__input--filled');
+        }        
+      })
+      .focus(function() {});   
+     
+    /* END NICE INPUT*/
+    
+    /* PARTNERS SLIDER DESKTOP*/    
+    
+   
+    $('.js-slider-partners').slick({
+        infinite: true,
+        arrows:false,
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+     
+    
+    $('.js-slider-partners-next').on('click', function(){
+        $('.js-slider-partners').slick('slickNext');
+        return false;
+    });
+    
+    $('.js-slider-partners-prev').on('click', function(){
+        $('.js-slider-partners').slick('slickPrev');
+        return false;
+    });
+     
+    $('.js-slider-partners')
+        .on('afterChange', function(event, slick, currentSlide, nextSlide){
+        var i=currentSlide+1;
+            $('.js-slider-num span').html(i);
+    });    
+    
+    /*END PARTNERS SLIDER DESKTOP*/
+    
+    /* PARTNERS SLIDER TABLET*/     
+    $('.js-slider-partners-2').slick({
+        infinite: true,
+        arrows:false,
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        });
+     
+    $('.js-slider-partners-next-2').on('click', function(){
+        $('.js-slider-partners-2').slick('slickNext');
+        return false;
+    });
+    
+    $('.js-slider-partners-prev-2').on('click', function(){
+        $('.js-slider-partners-2').slick('slickPrev');
+        return false;
+    });
+     
+    $('.js-slider-partners-2')
+        .on('afterChange', function(event, slick, currentSlide, nextSlide){
+        var i=currentSlide+1;
+            $('.js-slider-num-2 span').html(i);
+    });    
+    
+    /*END PARTNERS SLIDER TABLET*/
+    
+    
+    /* SOLUTIONS SLIDER*/ 
+    $('.js-slider-solutions').on('init', function(event, slick){
+        $('.slider-solutions-dots li').removeClass('active');
+        $('.slider-solutions-dots li[data-slide="00"]').addClass('active');
+    });
+    $('.js-slider-solutions').slick({
+            infinite: true,
+            arrows:true,
+            dots:false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade:true,
+        });
+    
+     $('.slider-solutions-dots li').on('click', function(){
+         var ds=$(this).attr('data-slide');
+        $('.js-slider-solutions').slick('slickGoTo', ds);
+         $('.slider-solutions-dots li').removeClass('active');
+         $(this).addClass('active');
+        return false;
+    });  
+     
+     $('.js-slider-solutions').on('beforeChange', function(event, slick, currentSlide, nextSlide){        
+        $('.slider-solutions-dots li').removeClass('active');
+        $('.slider-solutions-dots li[data-slide="0'+nextSlide+'"]').addClass('active');         
+    });
+    
+    /*END SOLUTIONS SLIDER*/
+    
+    /* NEWS SLIDER*/ 
+    
+    options={
+        infinite: false,
+        arrows:true,
+        dots:false,
+        rows:2,
+        slidesPerRow:2,
+        appendArrows:$('.slider-news-arrows'),
+            responsive: [
+                {
+                  breakpoint: 992,
+                  settings: {
+                    rows:2,
+                    slidesPerRow:1,
+                }
+                }
+              ]
+
+        }
+    
+    $('.js-slider-news').slick(options);
+    $('.slider-press-tabs__item').on('click', function(element){
+        var f=$(this).attr('data-filter');
+        
+        if(!$(this).hasClass('active')){
+            
+            if(f!=0){
+                
+                if($('.js-slider-news').hasClass('slick-initialized')){
+                    $('.js-slider-news').slick('unslick');
+                }
+                
+                if($('.js-slider-news-2').hasClass('slick-initialized')){
+                    $('.js-slider-news-2').slick('unslick');
+                }
+              
+                $('.js-slider-news').hide();
+                $('.js-slider-news-2').show();
+                $('.js-slider-news-2').html($('.js-slider-news').html());
+                $('.js-slider-news-2').find('.slider-news__slide').not('.'+f).remove();        
+                $('.js-slider-news-2').slick(options);    
+            }
+            else{
+                $('.js-slider-news').show();
+                $('.js-slider-news').slick(options);    
+                $('.js-slider-news-2').hide();
+            }
+            $('.slider-press-tabs__item').removeClass('active');
+            $(this).addClass('active');
+        }       
+         return false;
+    });
+ 
+    /*END NEWS SLIDER*/
+    
+     $('.mobile-trigger').on('click', function(event) {
+        if(!$('body').hasClass('menu-open')){
+            event.preventDefault();		
+            $('body').addClass('menu-open');
+            $('.sidebar').fadeIn(300);
+            $(this).addClass('is-active');
+        }else{
+            $('body').removeClass('menu-open');
+            $('.sidebar').fadeOut(300); 
+            $(this).removeClass('is-active');
+        }
+	});
+    
+    $('.js-slider-production').slick({
+        infinite: false,
+        arrows:true,
+        dots:false,
+        rows:2,
+        slidesPerRow:3,
+        appendArrows:$('.slider-production-arrows'),
+            responsive: [
+                {
+                  breakpoint: 9999,
+                  settings:"unslick" 
+                },
+                {
+                  breakpoint: 992,
+                  settings: {
+                    rows:2,
+                    slidesPerRow:3,
+                  }
+                },
+                {
+                  breakpoint: 641,
+                  settings: {
+                    rows:2,
+                    slidesPerRow:2,
+                  }
+                }
+              ]
+
+        });
+    
+    
+    
+    $("#form-partners").validate({
+         errorElement:'div',
+         errorPlacement: function(error, element) {
+            element.parent().append(error);
+        },
+			rules: {
+				name: "required",
+				phone: "required"                				
+			},
+			messages: {
+				name: "Необходимо заполнить «Имя».",
+				phone: "Необходимо заполнить «Телефон»."							
+			},
+            submitHandler: function(){}
+     });
+    
+    $("#form-subscribe").validate({
+         errorElement:'div',
+         errorPlacement: function(error, element) {
+            element.parent().append(error);
+        },
+			rules: {
+				email: {
+					required: true,
+					email: true
+				}	               				
+			},
+			messages: {
+				email: {
+                    required: "Необходимо заполнить «E-mail».",
+					email: "Введите корректный адрес электронной почты."
+                }								
+			},
+            submitHandler: function(){}
+     });
+
+    
+     
+ });
